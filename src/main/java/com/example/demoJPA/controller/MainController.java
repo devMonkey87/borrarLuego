65f7732af989758c67fb0d50fd2646a7b6364ead
devMonkey87 <jlandetemunoz@icloud.com>
2018-11-13 08:23:36 +0100

    Update MainController.java

--- src/main/java/com/example/demoJPA/controller/MainController.java
+++ src/main/java/com/example/demoJPA/controller/MainController.java
@@ -20,13 +20,16 @@ public class MainController {
 
 		List<Persona> entidades = new ArrayList<>();
 
-		Persona persona1 = new Persona("41004940b", "Spiderman", "random");
-		Persona persona2 = new Persona("31004940b", "Superman", "random");
-		Persona persona3 = new Persona("54004940b", "Antman", "random");
-
+		Persona persona1 = new Persona("41004940b", "Luke Skywalker", "Jedi");
+		Persona persona2 = new Persona("31004940b", "Han Solo", "Cazarrecompensas");
+		Persona persona3 = new Persona("54004940b", "Lord Vader", "Sith");
+Persona persona4 = new Persona("54004982b", "Lord Vader", "Sith");
+		Persona persona5 = new Persona("54344940x", "Maestro Yoda", "Jedi");
 		entidades.add(persona1);
 		entidades.add(persona2);
 		entidades.add(persona3);
+		entidades.add(persona4);
+		entidades.add(persona5);
 
 		return entidades;
 
@@ -37,12 +40,16 @@ public class MainController {
 	public List<Coche> listarTodosCoches() {
 
 		List<Coche> vehiculos = new ArrayList<>();
-		Coche coche1 = new Coche("xxxx", "Seat");
-		Coche coche2 = new Coche("fddffd", "Renault");
-		Coche coche3 = new Coche("sdsdd", "Ford");
+		Coche coche1 = new Coche("Modelo: Toledo", "Seat");
+		Coche coche2 = new Coche("Modelo: Megane", "Renault");
+		Coche coche3 = new Coche("Modelo: Fusion", "Ford");
+		Coche coche4 = new Coche("Modelo: Benz", "Mercedes");
+		Coche coche5 = new Coche("Modelo: Cooper", "Mini");
 		vehiculos.add(coche1);
 		vehiculos.add(coche2);
 		vehiculos.add(coche3);
+		vehiculos.add(coche4);
+		vehiculos.add(coche5);
 
 		return vehiculos;
 	}
