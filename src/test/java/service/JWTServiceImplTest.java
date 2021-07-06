5fdd6bf733b3f636261526384e431c0e7e687cdd
devMonkey87 <jlandetemunoz@icloud.com>
2021-07-06 20:38:51 +0200

    :test: add test

---
+++ src/test/java/service/JWTServiceImplTest.java
@@ -0,0 +1,19 @@
+package service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JWTServiceImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetUsername() {
+		assertTrue(true);
+	}
+
+}
